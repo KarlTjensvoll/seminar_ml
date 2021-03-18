057a440fc723fa2425495bea043fa1a05dd790f3
Karl Wilhelm <karl_tjensvoll@live.no>
2021-03-18 15:41:15 +0100

    Created function to load data, started on logit

---
+++ code/python/data_handler.py
@@ -0,0 +1,57 @@
+import os
+import pandas as pd
+from sklearn import model_selection as skm
+from sklearn.experimental import enable_iterative_imputer
+import sklearn.impute as ski
+
+
+def load_data(year: int, path='../../data', **kwargs) -> tuple:
+    """Loads x and y for the given year.
+
+    Args:
+        year (int): What year to load.
+        path (str, optional): Relative or absolute path to folder that has the
+        train folder. Defaults to '../../data'.
+
+    Raises:
+        ValueError: If you do not give a valid year.
+
+    Returns:
+        tuple: Returns the x_train and y_train for given year.
+    """
+    if year < 1 or year > 5:
+        raise ValueError("Year has to be in the range between 1 and 5")
+    
+    sep = ','
+    train_path = os.path.join(path, 'train')    
+    test_path = os.path.join(path, 'test')  
+    x = '/x_' + str(year) + 'year.txt'
+    y = '/y_' + str(year) + 'year.txt'
+    x_train = pd.read_csv(train_path + x, sep, **kwargs)
+    y_train = pd.read_csv(train_path + y, sep, **kwargs)
+    x_test = pd.read_csv(test_path + x, sep, **kwargs)
+    y_test = pd.read_csv(test_path + y, sep, **kwargs)
+    return x_train, x_test, y_train, y_test
+
+
+def split_train_validate(x:pd.DataFrame, y:pd.DataFrame, seed=42) -> tuple:
+    test_threshold = 4000
+    if x.shape[0] < test_threshold:
+        raise ValueError(f'Warning, frame has less than {test_threshold} rows, are you trying to split a test sample, instead of train sample?')
+    else:
+        return skm.train_test_split(
+            x,
+            y,
+            test_size=0.2,
+            random_state=seed,
+            shuffle=True,
+            stratify=y
+        )
+
+
+def impute_frame(x:pd.DataFrame, random_state=42, **kwargs) -> pd.DataFrame:
+    imputer = ski.IterativeImputer(random_state=random_state, **kwargs)
+    imputer.fit(x)
+    return imputer.transform(x)
+
+
--- code/python/split_data.py
+++ code/python/split_data.py
@@ -1,7 +1,8 @@
-from sklearn import model_selection
+from sklearn import model_selection as skm
 import numpy as np
 import pandas as pd
 import os
+import data_handler
 
 # from sklearn.utils import shuffle
 
@@ -12,7 +13,10 @@ def split_data(
     raw_path: str, train_path: str, test_path: str, seed: int
     ) -> None:
     """Loop over all the files in raw_path that ends with .txt. Then split it
-    into train and test data sets, with 20% of data split into train.
+    into train and test data sets, with 20% of data split into train. Then
+    save them in train and test folders respectively, as .txt files. They will
+    have name x_1, x_2, ... or y_1, y_2, ... etc. where the number corresponds
+    to the year.
 
     Args:
         raw_path (str): Path that has the raw data files.
@@ -32,7 +36,7 @@ def split_data(
 
             # We stratify on y, to make sure that we have proportionally
             # the same amount of bankrupt firm in train and test.
-            x_train, x_test, y_train, y_test = model_selection.train_test_split(
+            x_train, x_test, y_train, y_test = skm.train_test_split(
                 x, 
                 y, 
                 test_size=0.2, 
@@ -44,14 +48,33 @@ def split_data(
             # train and test.
             assert np.isclose(y_train.mean(), y_test.mean(), atol=0.0005)
             
+            # Impute missing. We do this seperately, so that no information
+            # from train data spills over to test data.
+            max_iter = 15
+            add_indicator = False
+            
+            print(f"Imputing train for {file}.")
+            x_train = data_handler.impute_frame(
+                x_train, max_iter=max_iter, add_indicator=add_indicator
+            )
+            print(f"Imputing test for {file}.")
+            x_test = data_handler.impute_frame(
+                x_test, max_iter=max_iter, add_indicator=add_indicator
+            )
+            
+            # Not very efficient, but the imputer transforms DataFrame to
+            # array, so we transform it back to DataFrame
+            x_train = pd.DataFrame(x_train)
+            x_test = pd.DataFrame(x_test)
+            
             save_x_train = os.path.join(train_path, 'x_' + file)
             save_y_train = os.path.join(train_path, 'y_' + file)
-            x_train.to_csv(save_x_train)
-            y_train.to_csv(save_y_train)
+            x_train.to_csv(save_x_train, index=False)
+            y_train.to_csv(save_y_train, index=False)
             save_x_test = os.path.join(test_path, 'x_' + file)
             save_y_test = os.path.join(test_path, 'y_' + file)
-            x_test.to_csv(save_x_test)
-            y_test.to_csv(save_y_test)
+            x_test.to_csv(save_x_test, index=False)
+            y_test.to_csv(save_y_test, index=False)
 
 
 # Seed is taken from random.org
